9b41a1dcd1d53b7ed80c75fcb737f2af93694c71
Brad <brad@viziworx.com>
2019-10-18 11:55:03 -0400

    Added useState to track data from useEffect

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+	// TODO: Add useState to track data from useEffect
+	const [ character, setCharacter ] = useState([]);
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+	useEffect(() => {
+		// TODO: Add API Request here - must run in `useEffect`
+		//  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+		axios.get('https://rickandmortyapi.com/api/character/').then((response) => {
+			console.log('Rick and Morty Characters:', response.data.results);
+			setCharacter(response.data.results);
+		});
+	}, []);
 
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+	if (!character) {
+		return <h1>Loading...</h1>;
+	}
+
+	return (
+		<section className='character-list'>
+			<h2>TODO: `array.map()` over your state here!</h2>
+		</section>
+	);
 }
