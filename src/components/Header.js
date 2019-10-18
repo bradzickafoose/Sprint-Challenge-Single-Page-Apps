c72489e5768d77804432e395e13f9c7195cd6eda
Brad <brad@viziworx.com>
2019-10-18 14:22:49 -0400

    Added NavLinks and styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function Header() {
+	const NavBar = styled.nav`
+		display: flex;
+		justify-content: space-between;
+		margin: 0 auto;
+		width: 90%;
+	`;
 	return (
 		<header className='ui centered'>
 			<h1 className='ui center'>Rick &amp; Morty Fan Page</h1>
+			<NavBar>
+				<NavLink class='nav-link' to='/'>
+					Home
+				</NavLink>
+				<NavLink class='nav-link' to='/characters'>
+					Characters
+				</NavLink>
+				<NavLink class='nav-link' to='/locations'>
+					Locations
+				</NavLink>
+				<NavLink class='nav-link' to='/episodes'>
+					Episodes
+				</NavLink>
+			</NavBar>
 		</header>
 	);
 }
