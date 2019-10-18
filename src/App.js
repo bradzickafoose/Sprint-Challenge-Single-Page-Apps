55454c36e643546e1f7530760084a94ea1f7029e
Brad <brad@viziworx.com>
2019-10-18 14:28:53 -0400

    Setup Routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Route } from 'react-router-dom';
 import Header from './components/Header';
 import WelcomePage from './components/WelcomePage';
 import CharacterList from './components/CharacterList';
+import LocationsList from './components/LocationsList';
+import EpisodesList from './components/EpisodesList';
 
 export default function App() {
 	return (
 		<main>
 			<Header />
 			<Route exact path='/' component={WelcomePage} />
-			<Route exact path='/character' component={CharacterList} />
+			<Route path='/characters' component={CharacterList} />
+			<Route path='/locations' component={LocationsList} />
+			<Route path='/episodes' component={EpisodesList} />
 		</main>
 	);
 }
---
+++ src/components/EpisodesCard.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function EpisodesCard({ name, type, dimension, residents }) {
+	return <span>todo: location</span>;
+}
