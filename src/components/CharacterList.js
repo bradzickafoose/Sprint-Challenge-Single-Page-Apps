7463e479105eb5c7f591d92e615b1b03381231a6
Brad <brad@viziworx.com>
2019-10-18 12:13:59 -0400

    Setup a array map of characters

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,26 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
 	// TODO: Add useState to track data from useEffect
-	const [ character, setCharacter ] = useState([]);
+	const [ characters, setCharacters ] = useState([]);
 
 	useEffect(() => {
 		// TODO: Add API Request here - must run in `useEffect`
 		//  Important: verify the 2nd `useEffect` parameter: the dependancies array!
 		axios.get('https://rickandmortyapi.com/api/character/').then((response) => {
 			console.log('Rick and Morty Characters:', response.data.results);
-			setCharacter(response.data.results);
+			setCharacters(response.data.results);
 		});
 	}, []);
 
-	if (!character) {
+	if (!characters) {
 		return <h1>Loading...</h1>;
 	}
 
 	return (
 		<section className='character-list'>
-			<h2>TODO: `array.map()` over your state here!</h2>
+			{characters.map((character, index) => {
+				return <CharacterCard key={index} image={character.image} name={character.name} status={character.status} />;
+			})}
 		</section>
 	);
 }
