222bb5c70c27427df35b68aa3a035d324752acef
Brad <brad@viziworx.com>
2019-10-18 13:12:56 -0400

    Added a SearchForm component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Header from './components/Header.js';
+import Header from './components/Header';
+import SearchForm from './components/SearchForm';
 import CharacterList from './components/CharacterList';
 
 export default function App() {
 	return (
 		<main>
 			<Header />
+			<SearchForm />
 			<CharacterList />
 		</main>
 	);
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 export default function SearchForm() {
- 
-  return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
-  );
+	const [ query, setQuery ] = useState('');
+
+	const handleInputChange = (event) => {
+		setQuery(event.target.value);
+	};
+	return (
+		<section className='search-form'>
+			<form className='search'>
+				<input type='text' onChange={handleInputChange} value={query} name='name' placeholder='search by name' />
+			</form>
+		</section>
+	);
 }
